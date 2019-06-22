64a773d4466f49b2860c4b63fef739aaaa27ec98
Владимир Ярмоленко <48343986+Woloja@users.noreply.github.com>
2019-06-23 01:00:23 +0300

    Add files via upload

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,112 @@
 
+/*********************
+ * Section import custom functions
+ * Import examples:
+ *********************/
+import {
+    // readCookie,
+    // createCookie,
+    // FormValidation,
+    isTouchDevice,
+    onReady,
+    onResize
+} from './delta-functions';
+
+
+/*********************
+ * initialize some site properties
+ *********************/
+if (!window.site.hasOwnProperty('scrollBarWidth')){
+    window.site.scrollBarWidth = window.innerWidth - document.body.scrollWidth;
+}
+if (!window.site.hasOwnProperty('supportsVibrate')) {
+    window.site.supportsVibrate = "vibrate" in navigator;
+}
+if (!window.site.hasOwnProperty('isTouch')) {
+    window.site.isTouch = isTouchDevice();
+}
+
+
+/*********************
+ * Section import jquery and libs
+ * Import examples:
+ *********************/
+// import $ from "jquery";
+// window.$ = window.jQuery = $;
+// import "jquery-ui/ui/widgets/tabs";
+
+
+/*********************
+ * Section import vanilla JS libs
+ * Import examples:
+ *********************/
+// import { TweenMax } from "gsap/TweenMax";
+// import TimelineMax from "gsap/TimelineMax"
+// import ScrollToPlugin from "gsap/ScrollToPlugin"
+// let scrollTo = ScrollToPlugin;
+// import ScrollMagic from 'ScrollMagic';
+// import 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
+
+
+/*********************
+ * Section Initialize some constant and manipulate DOM
+ * Initialize examples:
+ *********************/
+// if (!site.isTouch) { } else { }
+// function initFormValidation(object) {
+//     new FormValidation(object);
+// }
+// window.initFormValidation = initFormValidation;
+
+// if you use jQuery
+$(document)
+    .ajaxError(function(event, request, settings) {})
+    .ajaxStart(function(event) {})
+    .ajaxSuccess(function(event) {})
+    .ready(function () {})
+    //if you need another instance
+    .ready(function () {});
+
+$(window)
+    .on('load', function () {
+        loadAndResize();
+        //etc
+    })
+    .on('resize', function () {
+        loadAndResize();
+        //etc
+    })
+    .on('scroll', function () {});
+
+// else if you use blessed JavaScript
+onReady(function() {
+
+});
+
+
+createEvent(window, 'resize', function (e) {
+    loadAndResize();
+});
+
+createEvent(window, 'load', function (e) {
+    loadAndResize();
+});
+
+createEvent(window, 'scroll', function (e) {
+    
+});
+
+/*********************
+ * Section for creating in file function
+ * eg:
+ *      rendering custom html from json,
+ *      load and resize some specific elements on page,
+ *      and other
+ * Initialize examples:
+ *********************/
+/**
+ *
+ * @param {*} parameter
+ */
+function someFunction(parameter) {}
+function loadAndResize(parameter) {}
